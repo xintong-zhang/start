b45bc8bb4de43da30acb0dc99131470d57c2dc4b
xintong <xz@zestfinance.com>
2013-02-20 15:03:39 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Start::Application.config.secret_token = 'b98ec7cab900cdc666e0f2c49c6495f50fae6b45a680df280c22ed9359afa73f9ea61affc0c547caaf8870af7d54cbdeba68e54c6e06ed117fbe652f46cd1472'
